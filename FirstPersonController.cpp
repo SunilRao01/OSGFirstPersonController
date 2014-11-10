5c5840a93322cca6d069b621c6995ad6f77e8a8c
Sunil <slysunil101@gmail.com>
2014-11-10 11:52:09 -0600

    Added a prototype HUD

--- FirstPersonController.cpp
+++ FirstPersonController.cpp
@@ -1,6 +1,6 @@
 #include "FirstPersonController.h"
 
-const float moveSpeed = 0.02;
+const float moveSpeed = 0.1;
 const float inputTimeInterval = 0.02;
 
 osg::Vec3d tempMov;
@@ -83,9 +83,6 @@ bool FirstPersonController::handle(const osgGA::GUIEventAdapter &ea, osgGA::GUIA
    eyePos += camRotation * tempMov;
    matrix.setTrans(eyePos);
 
-   // TODO: Print out timer for smoothing
-
-
     // Check [mainTimer.time % (time divisor) == 0]
    if (mainTimer.time_s() >= maxTick)
    {
@@ -95,4 +92,137 @@ bool FirstPersonController::handle(const osgGA::GUIEventAdapter &ea, osgGA::GUIA
 
     return false;
 }
+
+osg::Camera *FirstPersonController::createHUD()
+{
+    // Create new camera for HUD
+    osg::Camera *cam = new osg::Camera();
+
+    // Set the camera's projection matrix
+    cam->setProjectionMatrix(osg::Matrix::ortho2D(0, 1280, 0, 1024));
+
+    // Set the view matrix
+    cam->setReferenceFrame(osg::Transform::ABSOLUTE_RF);
+    cam->setViewMatrix(osg::Matrix::identity());
+
+    // Clear the camera's depth buffer
+    cam->setClearMask(GL_DEPTH_BUFFER_BIT);
+
+    // Draw subgraph after main camera view
+    cam->setRenderOrder(osg::Camera::POST_RENDER);
+
+    // Don't override main camera's event focus
+    cam->setAllowEventFocus(false);
+
+
+    // What the HUD camera will render
+    osg::Geode *geode = new osg::Geode();
+
+    std::string hudFont("fonts/Bretan.otf");
+
+    osg::StateSet* stateset = geode->getOrCreateStateSet();
+    stateset->setMode(GL_LIGHTING,osg::StateAttribute::OFF);
+
+    osg::Vec3 position(150.0f,800.0f,0.0f);
+    osg::Vec3 delta(0.0f,-120.0f,0.0f);
+
+    {
+        osgText::Text *text = new  osgText::Text();
+        geode->addDrawable(text);
+
+        text->setFont(hudFont);
+        text->setPosition(position);
+
+        text->setText("Virtual UIC");
+
+        position += delta;
+    }
+
+    {
+        osg::BoundingBox bb;
+        for (int i = 0; i < geode->getNumDrawables(); ++i)
+        {
+            bb.expandBy(geode->getDrawable(i)->getBound());
+        }
+
+        osg::Geometry *geom = new osg::Geometry();
+
+        osg::Vec3Array *vertices = new osg::Vec3Array();
+        float depth = bb.zMin() - 0.1;
+
+        vertices->push_back(osg::Vec3(bb.xMin(), bb.yMin(), depth));
+        vertices->push_back(osg::Vec3(bb.xMin(), bb.yMin(), depth));
+        vertices->push_back(osg::Vec3(bb.xMin(), bb.yMin(), depth));
+        vertices->push_back(osg::Vec3(bb.xMin(), bb.yMin(), depth));
+        geom->setVertexArray(vertices);
+
+        osg::Vec3Array *normals = new osg::Vec3Array();
+        normals->push_back(osg::Vec3(0.0f, 0.0f, 1.0f));
+        geom->setNormalArray(normals, osg::Array::BIND_OVERALL);
+
+        osg::Vec4Array *colors = new osg::Vec4Array();
+        colors->push_back(osg::Vec4(1.0f, 1.0f, 0.8f, 0.2f));
+        geom->setColorArray(colors, osg::Array::BIND_OVERALL);
+
+        geom->addPrimitiveSet(new osg::DrawArrays(GL_QUADS, 0, 4));
+
+        osg::StateSet *stateSet = geom->getOrCreateStateSet();
+        stateSet->setMode(GL_BLEND, osg::StateAttribute::ON);
+        stateSet->setRenderingHint(osg::StateSet::TRANSPARENT_BIN);
+
+        geode->addDrawable(geom);
+
+        cam->addChild(geode);
+    }
+
+    return cam;
+}
+
+bool FirstPersonController::performMovement()
+{
+    //StandardManipulator::performMovement();
+    //FirstPersonManipulator::performMovement();
+
+    // return if less then two events have been added
+    if( _ga_t0.get() == NULL || _ga_t1.get() == NULL )
+        return false;
+
+    // get delta time
+    double eventTimeDelta = _ga_t0->getTime() - _ga_t1->getTime();
+    if( eventTimeDelta < 0. )
+    {
+        OSG_WARN << "Manipulator warning: eventTimeDelta = " << eventTimeDelta << std::endl;
+        eventTimeDelta = 0.;
+    }
+
+    // get deltaX and deltaY
+    float dx = _ga_t0->getXnormalized() - _ga_t1->getXnormalized();
+    float dy = _ga_t0->getYnormalized() - _ga_t1->getYnormalized();
+
+    // return if there is no movement.
+    if( dx == 0. && dy == 0. )
+        return false;
+
+    return performMovementLeftMouseButton( eventTimeDelta, dx, dy );
+    // call appropriate methods
+    unsigned int buttonMask = _ga_t1->getButtonMask();
+    if( buttonMask == osgGA::GUIEventAdapter::LEFT_MOUSE_BUTTON )
+    {
+        return performMovementLeftMouseButton( eventTimeDelta, dx, dy );
+    }
+    else if( buttonMask == osgGA::GUIEventAdapter::MIDDLE_MOUSE_BUTTON ||
+            buttonMask == (osgGA::GUIEventAdapter::LEFT_MOUSE_BUTTON | osgGA::GUIEventAdapter::RIGHT_MOUSE_BUTTON) )
+    {
+        return performMovementMiddleMouseButton( eventTimeDelta, dx, dy );
+    }
+    else if( buttonMask == osgGA::GUIEventAdapter::RIGHT_MOUSE_BUTTON )
+    {
+        return performMovementRightMouseButton( eventTimeDelta, dx, dy );
+    }
+
+    return false;
+
+
+}
+
 
--- FirstPersonController.h
+++ FirstPersonController.h
@@ -6,6 +6,7 @@
 #include <osgViewer/Viewer>
 #include <osgGA/GUIEventHandler>
 #include <osg/Timer>
+#include <osgText/Text>
 #include <iostream>
 #include <math.h>
 #include <stdlib.h>
@@ -16,25 +17,52 @@ class FirstPersonController : public osgGA::FirstPersonManipulator
 public:
    FirstPersonController(osgViewer::Viewer *inputViewer) : _viewer(inputViewer)
    {
-        mainTimer.setStartTick();
-        //_viewer->setCameraManipulator(new FirstPersonPerspective);
-        //_viewer->setCameraManipulator(new osgGA::FirstPersonManipulator);
+       // Start frame timer
+       mainTimer.setStartTick();
+
+
+       // Add HUD
+       _viewer->setUpViewAcrossAllScreens();
+       osgViewer::Viewer::Windows windows;
+       _viewer->getWindows(windows);
+
+       if (windows.empty())
+       {
+           std::cout << "No windows for viewer found...";
+       }
+
+       osg::Camera *hudCam = createHUD();
+
+       hudCam->setGraphicsContext(windows[0]);
+       hudCam->setViewport(0, 0, windows[0]->getTraits()->width, windows[0]->getTraits()->height);
+
+       _viewer->addSlave(hudCam, false);
    }
 
+   // Overload unnecessary functions from FirstPersonManipulator
    bool handleMouseWheel( const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us )
    {
        return true;
    };
 
+   // Override StandardManipulator's performMovment function to get rid of requirement
+   // to hold left mouse button to look around
+   virtual bool performMovement();
+
+   // Override handle function for keyboard input
    virtual bool handle(const osgGA::GUIEventAdapter &ea, osgGA::GUIActionAdapter &);
 
    virtual void accept(osgGA::GUIEventHandlerVisitor &v)
     {
         v.visit(*this);
     };
+
+   osg::Camera *createHUD();
 protected:
    osgViewer::Viewer *_viewer;
    osg::Timer mainTimer;
+
+
 };
 
 #endif // FIRSTPERSONCONTROLLER_H
