bddd1e4c40fd7ef0caf775262de2707854070b09
Sunil <slysunil101@gmail.com>
2014-12-11 21:35:44 -0600

    FIxed speed

--- FirstPersonController.cpp
+++ FirstPersonController.cpp
@@ -1,6 +1,6 @@
 #include "FirstPersonController.h"
 
-const float moveSpeed = 0.02;
+const float moveSpeed = 0.6;
 const float inputTimeInterval = 0.02;
 
 osg::Vec3d tempMov;
@@ -17,6 +17,7 @@ bool FirstPersonController::handle(const osgGA::GUIEventAdapter &ea, osgGA::GUIA
        return false;
    }
 
+   // Set the viewer's "eye" position, which is located at the center of the camera.
    osg::Vec3d eyePos;
    osg::Matrix matrix = _viewer->getCameraManipulator()->getMatrix();
 
@@ -28,7 +29,7 @@ bool FirstPersonController::handle(const osgGA::GUIEventAdapter &ea, osgGA::GUIA
    {
       case(osgGA::GUIEventAdapter::KEYDOWN):
       {
-         // Relies on keyboard input
+         // Movement of the camera correlates with W, A, S, D
          switch(ea.getKey())
          {
              case 'w':
@@ -83,9 +84,6 @@ bool FirstPersonController::handle(const osgGA::GUIEventAdapter &ea, osgGA::GUIA
    eyePos += camRotation * tempMov;
    matrix.setTrans(eyePos);
 
-   // TODO: Print out timer for smoothing
-
-
     // Check [mainTimer.time % (time divisor) == 0]
    if (mainTimer.time_s() >= maxTick)
    {
@@ -93,6 +91,55 @@ bool FirstPersonController::handle(const osgGA::GUIEventAdapter &ea, osgGA::GUIA
        maxTick += inputTimeInterval;
    }
 
+
     return false;
 }
+
+bool FirstPersonController::performMovement()
+{
+    // return if less then two events have been added
+    if( _ga_t0.get() == NULL || _ga_t1.get() == NULL )
+    {
+        return false;
+    }
+
+    // get delta time, throw warning if error with delta time
+    double eventTimeDelta = _ga_t0->getTime() - _ga_t1->getTime();
+    if( eventTimeDelta < 0. )
+    {
+        OSG_WARN << "Manipulator warning: eventTimeDelta = " << eventTimeDelta << std::endl;
+        eventTimeDelta = 0.;
+    }
+
+    // get deltaX and deltaY
+    float dx = _ga_t0->getXnormalized() - _ga_t1->getXnormalized();
+    float dy = _ga_t0->getYnormalized() - _ga_t1->getYnormalized();
+
+    // return if there is no movement.
+    if( dx == 0.0 && dy == 0.0 )
+    {
+        return false;
+    }
+
+    return performMovementLeftMouseButton( eventTimeDelta, dx, dy);
+
+    // call appropriate methods
+    unsigned int buttonMask = _ga_t1->getButtonMask();
+    if( buttonMask == osgGA::GUIEventAdapter::LEFT_MOUSE_BUTTON )
+    {
+        return performMovementLeftMouseButton( eventTimeDelta, dx, dy );
+    }
+    else if( buttonMask == osgGA::GUIEventAdapter::MIDDLE_MOUSE_BUTTON ||
+            buttonMask == (osgGA::GUIEventAdapter::LEFT_MOUSE_BUTTON | osgGA::GUIEventAdapter::RIGHT_MOUSE_BUTTON) )
+    {
+        return performMovementMiddleMouseButton( eventTimeDelta, dx, dy );
+    }
+    else if( buttonMask == osgGA::GUIEventAdapter::RIGHT_MOUSE_BUTTON )
+    {
+        return performMovementRightMouseButton( eventTimeDelta, dx, dy );
+    }
+
+    return false;
+}
+
 
--- FirstPersonController.h
+++ FirstPersonController.h
@@ -6,35 +6,45 @@
 #include <osgViewer/Viewer>
 #include <osgGA/GUIEventHandler>
 #include <osg/Timer>
+#include <osgText/Text>
 #include <iostream>
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
+
 
 class FirstPersonController : public osgGA::FirstPersonManipulator
 {
 public:
    FirstPersonController(osgViewer::Viewer *inputViewer) : _viewer(inputViewer)
    {
-        mainTimer.setStartTick();
-        //_viewer->setCameraManipulator(new FirstPersonPerspective);
-        //_viewer->setCameraManipulator(new osgGA::FirstPersonManipulator);
+       // Start frame timer
+       mainTimer.setStartTick();
+
+
    }
 
+   // Overload unnecessary functions from FirstPersonManipulator
    bool handleMouseWheel( const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us )
    {
        return true;
    };
 
+   virtual bool performMovement();
+
+   // Override handle function for keyboard input
    virtual bool handle(const osgGA::GUIEventAdapter &ea, osgGA::GUIActionAdapter &);
 
    virtual void accept(osgGA::GUIEventHandlerVisitor &v)
     {
         v.visit(*this);
     };
+
 protected:
    osgViewer::Viewer *_viewer;
    osg::Timer mainTimer;
+
+
 };
 
 #endif // FIRSTPERSONCONTROLLER_H
